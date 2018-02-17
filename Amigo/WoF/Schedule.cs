1efcdcab5de0b71bd5d005fcefe817a3b21eac00
Andrei Roca <rocaandrei@users.noreply.github.com>
2018-02-17 12:13:28 +0200

    to do TeamAR

--- Amigo/WoF/Schedule.cs
+++ Amigo/WoF/Schedule.cs
@@ -14,8 +14,9 @@ namespace WeellOfFate
         {
 
         }
-        public virtual void GetSchedule()
+        public virtual void GetSchedule()// LUCRAM CU ASTA 
         {
+            //luam info din noi luam din team - > adaugam in Schedule
 
         }
     }

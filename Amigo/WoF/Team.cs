4c76d7ca8ca9fdaf5cd555054bd1f278e05c4c3d
oana.buturas <oana.buturas@gmail.com>
2018-02-19 19:28:57 +0200

    "Read from file"

--- Amigo/WoF/Team.cs
+++ Amigo/WoF/Team.cs
@@ -10,5 +10,29 @@ namespace WeellOfFate
     {
         public List<Engineer> Engineers;
         public Manager Manager;
+
+        public Team()
+        {
+            Engineers = new List<Engineer>();
+        }
+
+        public void ReadFromFile(string fileName)
+        {
+            StreamReader sr = new StreamReader(fileName);
+            string text = sr.ReadToEnd();
+            int[] names = new int[] { text.Split(",") };
+
+            id = 0;
+            for (int i = 0; i < names.Length; i++)
+            {
+                name = names[i];
+                role = "Engineer";
+                id++;
+                Engineers.Add(new Engineer(names, role, id));
+            }
+
+            sr.Close();
+        }
     }
+
 }

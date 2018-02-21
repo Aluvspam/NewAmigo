53098701c619c87d10524789dda3c693ef6a18eb
Dana <danaciocoiu78@gmail.com>
2018-02-21 11:54:47 +0200

    Incercare Dana

--- Amigo/WoF/Schedule.cs
+++ Amigo/WoF/Schedule.cs
@@ -17,35 +17,62 @@ namespace WeellOfFate
         {
 
         }
-        public virtual void GetSchedule(List<Engineer> engineers )//Schedule following business rules
+       /* public virtual void GetSchedule(List<Engineer> engineers)//Schedule following business rules
         {
-           
             //next: Business Rules
-
             foreach (var item in engineers)
             {
-               if (item.id%2 == 0)//reglua: sa nu lucreze doua zile consecutive
-                {
-
-               }
-               else if (Shift.)
+                if (item.id % 2 == 0)//regula: sa nu lucreze doua zile consecutive
                 {
 
                 }
-               else if (true)
-               {
+                else if (Shift.)
+                {
 
-               }
+                }
+                else if (true)
+                {
+
+                }
             }
+        }*/
 
-
-
-            //next: Random Select Engineers 
-
-            //next: Export Engineers
-
-            //next: Shift 1/ Shift 2
-
+        //next: Random Select Engineers 
+        //next: Export Engineers
+        //next: Shift 1/ Shift 2
+        //Incercare Dana
+        public virtual void GetSchedule(List<Engineer> engineers)
+        {
+            foreach (var item in engineers)
+            {
+                if (item.id % 2 == 0)
+                {
+                   
+                    Console.WriteLine("Complete one half day shift");
+                }
+                else
+                { Console.WriteLine("One half day shift completed");
+                }
+                    
+                else if (item.id >= 10)
+                {
+                    Console.WriteLine("Complete one whole day of support");
+                }
+               
+                Random random = new Random();
+                int randomID = random.Next();
+            }
+        }
+        
+        public override string ToString()
+        {
+            var result = "";
+            foreach (var item in Shifts)//
+            {
+                result += "Shift: " + Shifts + Environment.NewLine;
+            }
+            return result;
         }
     }
 }
+

22e88b856e15c1ece8f94e06b669102158bab8dd
andrei patrascu <andreipatrascu@outlook.com>
2018-02-26 18:57:32 +0200

    created CreateRandomSchedule()

--- Amigo/WoF/Schedule.cs
+++ Amigo/WoF/Schedule.cs
@@ -20,11 +20,11 @@ namespace WeellOfFate
         public virtual void GetSchedule(List<Engineer> engineers)//Schedule following business rules
         {
             //next: Random Select Engineers 
-           
+
             Random random = new Random(0);
             var r1 = random.Next(engineers.Count);//imi selecteaza random dupa numarul de ingineri
-            Engineer en1 = new Engineer("Gita", 0, "Inginer");// setam un inginer initial cu id-ul 0
-            if (r1%2 !=0)
+            Engineer en1 = new Engineer("Gita", 0, "Inginer");// setam un inginer initial cu id-ul 0 TO DO: do we really need this?
+            if (r1 % 2 != 0)
             {
                 en1.id = r1;// daca valoarea este reala, schimbam id-ulinginerului cu val din r1
             }
@@ -32,9 +32,21 @@ namespace WeellOfFate
 
             List<Engineer> RandomSelectedEngineers = new List<Engineer>();// lista cu ingineri random
             RandomSelectedEngineers.Add(en1);//adauga in lista 
-            
+
             //next: Export Engineers
             //next: Shift 1/ Shift 2
+
+            //incercare andrei
+            //we asume that we created shifts in chronological order if last shift is 2 weeks from now we already created the schedule and we can return it, else we need to create it now
+            if (Shifts[Shifts.Count - 1].StartTime.Day < DateTime.Now.Day + 14)
+            {
+                CreateRandomSchedule();
+            }
+        }
+        //populates shifts
+        private void CreateRandomSchedule()
+        {
+            throw new NotImplementedException();
         }
 
         //Incercare Dana
